fc27c38207a4b84e039a835f7dfab01ad4efa41a
murilloguilherme <murillo.guilherme3101@gmail.com>
2025-07-22 17:40:03 -0300

    Initial javascript

---
+++ scripts.js
@@ -0,0 +1,40 @@
+// Seleciona os elementos do formulário.
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+
+// Captura o evento de input para formartar o valor 
+amount.oninput = () => {
+    // Obtem o valor atual do input e remove os caracteres não numéricos
+    let value = amount.value.replace(/\D/g, "") 
+    // transformar o valor em centavos (exemplo: 150/100 = 1.5 que é equivalente a R$ 1,50)
+    value = Number(value) / 100
+    // Atualiza o valor do input
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+    // Formata o valor no pad~rao BRL (Real Brasileiro)
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+    // Retorna o valor formatado
+    return value
+}
+
+// Captura o evento de submnit do formulário para obter os valores.
+form.onsubmit = (event) => {
+    event.preventDefault() // Previne o comportamento padrão de recarregar a pagina
+    // Cria um objeto com os detalhes na nova despesa
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value, 
+        created_at: new Date(),
+    }
+    console.log(newExpense)
+}
